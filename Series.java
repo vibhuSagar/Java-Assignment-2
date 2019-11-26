a78b7c5bcff189e58d174ed1b857961cba383a14
Vibhu Sagar <vibhu.sagar96@gmail.com>
2019-11-26 16:45:04 +0530

    Add files via upload

---
+++ Series.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Series{
+
+    public static void printFibonancciSerices(int n) {
+        
+        int term1=0, term2=1, term3=1, term4=2;
+        int sum;
+        
+        System.out.println(term1);
+        System.out.println(term2);
+        System.out.println(term3);
+        System.out.println(term4);
+        
+        for(int i=4; i<n; i++){
+            sum=term1+term2+term3+term4;
+            
+            System.out.println(sum);
+            term1=term2;
+            term2=term3;
+            term3=term4;
+            term4=sum;
+        }
+
+    }
+
+
+
+    
+    public static void main(String[] args){
+        
+     Series s=new Series();
+        System.out.println("Enter the number of terms");
+        Scanner num = new Scanner(System.in);
+        int number=num.nextInt();
+        s.printFibonancciSerices(number);
+    }
+}
+
