2ff0cb4a4d5d65fe174e96df9a738f1284445171
Vibhu Sagar <vibhu.sagar96@gmail.com>
2019-11-27 11:52:53 +0530

    Update Series.java

--- Series.java
+++ Series.java
@@ -1,18 +1,22 @@
+/* Printing Fibonacci series */
+
 import java.util.Scanner;
 
 public class Series{
 
-    public static void printFibonancciSerices(int n) {
+    public static void printFibonancciSerices(int n) {      //method to print Fibonacci Series
         
         int term1=0, term2=1, term3=1, term4=2;
         int sum;
+        
+        /* printing the first four terms */
         
         System.out.println(term1);
         System.out.println(term2);
         System.out.println(term3);
         System.out.println(term4);
         
-        for(int i=4; i<n; i++){
+        for(int i=4; i<n; i++){                 //Calculating the sum of upcoming four terms
             sum=term1+term2+term3+term4;
             
             System.out.println(sum);
