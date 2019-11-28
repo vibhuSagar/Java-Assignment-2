374d34ee240b062cbfea9cfdfa1f19b1362d7501
Vibhu Sagar <vibhu.sagar96@gmail.com>
2019-11-28 20:59:10 +0530

    Create reverseNumber.java

---
+++ reverseNumber.java
@@ -0,0 +1,17 @@
+package test;
+
+
+
+public class reverseNumber {
+	
+	 public static void main(String[] args) {
+	        int num = 1234, reversed = 0;
+	        while(num != 0) {
+	            int digit = num % 10;
+	            reversed = reversed * 10 + digit;
+	            num /= 10;
+	        }
+	        System.out.println("Reversed Number: " + reversed);
+	}
+
+}
